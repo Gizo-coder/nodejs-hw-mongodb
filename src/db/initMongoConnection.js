e34567defe35f19ce020cfbfe828a50018ffad53
Gizem Demirci <gizdemirci91@gmail.com>
2025-11-11 18:04:20 +0300

    final uploading

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -11,7 +11,7 @@ export async function initMongoConnection() {
     if (!MONGODB_USER || !MONGODB_PASSWORD || !MONGODB_URL || !MONGODB_DB) {
         throw new Error('Missing MongoDB environment variables. Check .env'); 
     }
-    const connectionString = `mongodb+srv//${encodeURIComponent(MONGODB_USER)}:${encodeURIComponent(MONGODB_PASSWORD)}@${MONGODB_URL}/${MONGODB_DB}?retryWrites=true&w=majority`;
+    const connectionString = `mongodb+srv://${encodeURIComponent(MONGODB_USER)}:${encodeURIComponent(MONGODB_PASSWORD)}@${MONGODB_URL}/${MONGODB_DB}?retryWrites=true&w=majority`;
 
     try {
         await mongoose.connect(connectionString, {
